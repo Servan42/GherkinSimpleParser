f3c7a7fa745c292d6c574c207bcfc86620883748
Servan CHARLOT <charlot.servan@gmail.com>
2023-12-29 16:17:24 +0100

    feat: add console

---
+++ GherkinSimpleParser.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using GherkinSimpleParser;
+
+var directory = Console.ReadLine();
+Directory.CreateDirectory(@$"{directory}/GherkinToCsv");
+foreach (var filepath in Directory.GetFiles(directory).Where(f => f.Contains(".feature")))
+{
+    Console.WriteLine(filepath);
+    var lines = File.ReadAllLines(filepath).ToList();
+    var goCSV = GherkinObject.Parse(lines).ExportAsCSVWithExcelFormulaWrap_FR();
+    File.WriteAllLines(@$"{directory}/GherkinToCsv/{Path.GetFileName(filepath)}.csv", goCSV);
+}
