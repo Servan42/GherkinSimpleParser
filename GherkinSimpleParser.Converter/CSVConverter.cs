6b0aaa4d6990ce3851e864f2b6cf3c5a57150ba9
Servan42 <charlot.servan@gmail.com>
2024-01-18 02:14:03 +0100

    feat(Doc strings): Export CSV

--- GherkinSimpleParser.Converter/CSVConverter.cs
+++ GherkinSimpleParser.Converter/CSVConverter.cs
@@ -17,11 +17,7 @@ namespace GherkinSimpleParser.Converter
 
             if (gherkinObj.Background.Givens.Count > 0)
             {
-                string givenBackground = string
-                    .Join(separator, 
-                        gherkinObj.Background.Givens
-                        .Prepend(new Instruction("GENERAL PREREQUISITES:"))
-                        .Select(i => i.MainLine));
+                string givenBackground = $"GENERAL PREREQUISITES:{separator}{BuildInstructionBatchLine(separator, gherkinObj.Background.Givens)}";
                 csv.Add($";{givenBackground};;");
             }
 
@@ -29,14 +25,30 @@ namespace GherkinSimpleParser.Converter
             foreach (var scenario in gherkinObj.Scenarios)
             {
                 csv.Add($"{testCount};{scenario.Name};;");
-                string givenCol = string.Join(separator, scenario.Givens.Select(x => x.MainLine));
+                string givenCol = BuildInstructionBatchLine(separator, scenario.Givens);
                 string whenCol = scenario.When;
-                string thenCol = string.Join(separator, scenario.Thens.Select(x => x.MainLine));
+                string thenCol = BuildInstructionBatchLine(separator, scenario.Thens);
                 csv.Add($";{givenCol};{whenCol};{thenCol}");
                 testCount++;
             }
 
             return csv;
+        }
+
+        private string BuildInstructionBatchLine(string separator, List<Instruction> instructions)
+        {
+            var flattened = new List<string>();
+            foreach(var instruction in instructions)
+            {
+                var sb = new StringBuilder();
+                sb.Append(instruction.MainLine);
+                if(instruction.DocStrings.Count > 0)
+                {
+                    sb.Append(" \"").Append(string.Join(' ', instruction.DocStrings)).Append('"');
+                }
+                flattened.Add(sb.ToString());
+            }
+            return string.Join(separator, flattened);
         }
 
         public List<string> ExportAsCSVWithExcelFormulaWrap_FR(GherkinObject gherkinObj)
--- GherkinSimpleParser.Tests/ExportTests.cs
+++ GherkinSimpleParser.Tests/ExportTests.cs
@@ -88,6 +88,33 @@ namespace GherkinSimpleParser.Tests
         }
 
         [Test]
+        public void Should_export_as_CSV_withdocstrings()
+        {
+            // Given
+            var converter = new CSVConverter();
+            gherkinObject.Scenarios.First().Givens.First().DocStrings = new List<string>
+            {
+                "docstrings1",
+                "docstrings2",
+            };
+
+            // When
+            var csvResult = converter.ExportAsCSV("|", gherkinObject);
+
+            // Then
+            CollectionAssert.AreEqual(new List<string>()
+            {
+                "Number;GIVEN;WHEN;THEN",
+                ";GENERAL PREREQUISITES:|Prerequisite_0.1|Prere\"q\"uisite_0.2;;",
+                "1;Test Case 1;;",
+                ";Prerequisite_1.1 \"docstrings1 docstrings2\"|Prere\"q\"uisite_1.2;Action_1;Result_1.1|Resu\"l\"t_1.2",
+                "2;Test Case 2;;",
+                ";Prerequisite_2.1|Prere\"q\"uisite_2.2;Action_2;Result_2.1|Resu\"l\"t_2.2"
+            },
+            csvResult);
+        }
+
+        [Test]
         public void Should_export_as_CSV_excel_formula_wrap_french()
         {
             // Given
