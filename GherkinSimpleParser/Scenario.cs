08216c8a013cca3b8796b26d64099179206da45d
Servan42 <charlot.servan@gmail.com>
2024-01-25 23:00:43 +0100

    wip(markdown): Wrote tests for markdown

--- GherkinSimpleParser/Scenario.cs
+++ GherkinSimpleParser/Scenario.cs
@@ -3,6 +3,7 @@
     public class Scenario
     {
         public string Name { get; set; }
+        public List<string> MarkdownLines { get; set; } = new();
         public List<Instruction> Givens { get; set; } = new();
         public List<Instruction> Whens { get; set; } = new();
         public List<Instruction> Thens { get; set; } = new();
